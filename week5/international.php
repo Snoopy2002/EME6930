e8f1817c81633975275f67c6f0b1cf3e0f705d07
Giancarlo <40617195+Snoopy2002@users.noreply.github.com>
2022-02-28 20:34:58 -0500

    week version 1

---
+++ week5/international.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+Student First Name: <?php echo $_GET["fname"]; ?><br>
+Student Last Name: <?php echo $_GET["lname"]; ?><br>
+Student Age: <?php echo $_GET["age"]; ?><br>
+Student Gender: <?php echo $_GET["gender"]; ?><br> 
+Parent Email: <?php echo $_GET["email"]; ?><br> 
+<?php echo "Today is " . date("Y.m.d") . "<br>";
+?>
+</body>
+</html>
