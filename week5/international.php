4f2c1573ebf8f216d8a9b82eabf807c7b788dfa4
Giancarlo <40617195+Snoopy2002@users.noreply.github.com>
2022-02-28 21:13:29 -0500

    Update international.php

--- week5/international.php
+++ week5/international.php
@@ -1,12 +1,11 @@
 <html>
 <body>
-
+PHP output<br>
 Student First Name: <?php echo $_GET["fname"]; ?><br>
 Student Last Name: <?php echo $_GET["lname"]; ?><br>
 Student Age: <?php echo $_GET["age"]; ?><br>
 Student Gender: <?php echo $_GET["gender"]; ?><br> 
 Parent Email: <?php echo $_GET["email"]; ?><br> 
-<?php echo "Today is " . date("Y.m.d") . "<br>";
-?>
+<?php echo "Today is " .date("Y.m.d")?>
 </body>
 </html>
